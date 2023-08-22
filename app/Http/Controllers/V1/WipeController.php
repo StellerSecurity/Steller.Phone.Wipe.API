43d3dff3d382ef2ca2406adf42393daef9e15ae3
karimgtc <128481132+karimgtc@users.noreply.github.com>
2023-08-22 02:20:35 +0200

    Init

---
+++ app/Http/Controllers/V1/WipeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use Illuminate\Http\Request;
+
+class WipeController
+{
+
+    public function find(Request $request) {
+
+        // find wipe info by auth-token.
+
+    }
+
+}
--- app/Http/Controllers/V1/WipeUserController.php
+++ app/Http/Controllers/V1/WipeUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\V1;
 
+use Illuminate\Encryption\Encrypter;
 use Illuminate\Http\Request;
 
 class WipeUserController
@@ -9,6 +10,16 @@ class WipeUserController
 
     public function add(Request $request) {
 
+
+        // Our custom encryption key:
+        $key = 'U2x2QdvosFTtk5nL0ejrKqLFP1tUDtSt';
+
+        $encrypter = new Encrypter(
+            key: $key,
+            cipher: config('app.cipher'),
+        );
+
+        $encryptedValue = $encrypter->encrypt('hello');
 
     }
 
