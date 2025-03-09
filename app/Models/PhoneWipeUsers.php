01d8a512b258c258096426a48c4528a817b2f636
Bob <150491881+stellar-security-os@users.noreply.github.com>
2025-03-10 00:15:07 +0100

    Removed timestamps.

--- app/Models/PhoneWipeUsers.php
+++ app/Models/PhoneWipeUsers.php
@@ -15,4 +15,6 @@ class PhoneWipeUsers extends Model
 
     protected $fillable = ['username', 'password', 'auth_token', 'subscription_id', 'status', 'secret_key', 'key_helper', 'last_call', 'wiped_by'];
 
+    public $timestamps = false;
+
 }
