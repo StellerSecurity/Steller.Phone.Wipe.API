1ba8f3409c7d12f8517b203f1ba2894507974729
kalle <info@stellarsecurity.com>
2025-11-06 02:13:15 +0100

    Added insights.

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,17 @@ use App\Http\Controllers\V1\WipeUserController;
 use Mondago\ApplicationInsights\Middleware\TrackRequest;
 
 // v1
-Route::prefix('v1')->middleware(['basicAuth'])->group(function () {
-    Route::prefix('wipeusercontroller')->controller(WipeUserController::class)->group(function () {
-        Route::match(['get','post'], '/loginauth', 'auth');
-        Route::match(['get','post'], '/add', 'add');
-        Route::get('/findbytoken', 'findbytoken');
-        Route::get('/findbysubscriptionid', 'findbysubscriptionid');
-        Route::patch('/patch', 'patch');
+Route::prefix('v1')
+    ->middleware([TrackRequest::class, 'basicAuth']) // order matters
+    ->group(function () {
+        Route::prefix('wipeusercontroller')
+            ->controller(WipeUserController::class)
+            ->group(function () {
+                Route::match(['get','post'], '/loginauth', 'auth');
+                Route::match(['get','post'], '/add', 'add');
+                Route::get('/findbytoken', 'findbytoken');
+                Route::get('/findbysubscriptionid', 'findbysubscriptionid');
+                Route::patch('/patch', 'patch');
+            });
     });
-})->middleware(TrackRequest::class);
 
